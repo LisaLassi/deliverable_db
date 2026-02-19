312ac339b2d6d981c75154ec9d05f37de9ae97f0
[Lisa Lassi] <[lisa.lassi@live.se]>
2026-02-19 17:44:03 +0000

    adjusting spaces

--- deliverable_db/03-shop.sql
+++ deliverable_db/03-shop.sql
@@ -19,26 +19,26 @@ CREATE TABLE product_description (
 );
 
 CREATE TABLE products (
-        product_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        SKU VARCHAR(20) NOT NULL UNIQUE,
-        name VARCHAR(50) NOT NULL,
-        category_id INTEGER NOT NULL,
-        manufacturer_id INTEGER NOT NULL,
+	product_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	SKU VARCHAR(20) NOT NULL UNIQUE,
+	name VARCHAR(50) NOT NULL,
+	category_id INTEGER NOT NULL,
+	manufacturer_id INTEGER NOT NULL,
 	product_description_id INTEGER NOT NULL,
-        FOREIGN KEY (category_id) REFERENCES category(category_id),
-        FOREIGN KEY (manufacturer_id) REFERENCES manufacturer(manufacturer_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (manufacturer_id) REFERENCES manufacturer(manufacturer_id),
 	FOREIGN KEY (product_description_id) REFERENCES product_description(product_description_id)
 );
 
 CREATE TABLE product_sizes (
-        product_size_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        product_id INTEGER NOT NULL,
-        size VARCHAR(20) NOT NULL,
-        FOREIGN KEY (product_id) REFERENCES products(product_id)
+	product_size_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	product_id INTEGER NOT NULL,
+	size VARCHAR(20) NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
 CREATE TABLE product_colours (
-        product_colour_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	product_colour_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	product_id INTEGER NOT NULL,
 	colour VARCHAR(50) NOT NULL,
 	FOREIGN KEY (product_id) REFERENCES products(product_id)
